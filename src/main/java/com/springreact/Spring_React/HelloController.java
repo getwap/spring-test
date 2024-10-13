8f7c9dd0adcae98076a7013bb3813d400d67e2d6
Abel <abel@Abels-MacBook-Air.local>
2024-10-13 11:35:37 +0400

    initial commit: Springboot App

---
+++ src/main/java/com/springreact/Spring_React/HelloController.java
@@ -0,0 +1,14 @@
+package com.springreact.Spring_React;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "Hello from Spring Boot!";
+    }
+}
