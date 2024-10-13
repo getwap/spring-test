311bb62e1f65fbd81fbcb643874735f679d1fe98
Abel <abel@Abels-MacBook-Air.local>
2024-10-13 14:40:28 +0400

    controller removed

--- src/main/java/com/springreact/Spring_React/HelloController.java
+++ src/main/java/com/springreact/Spring_React/HelloController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello from Spring Boot! 😊";
-    }
 
     @GetMapping("/api/hello2")
     public String hello2() {
