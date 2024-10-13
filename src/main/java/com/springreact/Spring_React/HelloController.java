300e663ad9b5bf076dcad9c91d043aa58a8df4d7
Abel <abel@Abels-MacBook-Air.local>
2024-10-13 11:51:17 +0400

    secondary api creation

--- src/main/java/com/springreact/Spring_React/HelloController.java
+++ src/main/java/com/springreact/Spring_React/HelloController.java
@@ -11,4 +11,9 @@ public class HelloController {
     public String hello(){
         return "Hello from Spring Boot!";
     }
+
+    @GetMapping("/api/hello2")
+    public String hello2(){
+        return "Hello from Spring boot 2";
+    }
 }
